c08cecae215ab3ec0c328ce5a1b3ebe29d8a0f97
Alejandro Valverde <alejandrovalverdelopez@gmail.com>
2018-03-01 00:49:01 +0100

    Plotting of input/output plottings is working. It was not possible to identify a manner of extracting useful outputs by extending C

--- SH09_s_id/main.py
+++ SH09_s_id/main.py
@@ -8,12 +8,13 @@ from configClassModule import *
 
 #### SCRIPT
 cwd = os.getcwd()
+
 #Load plotting options
 plotSettings = plottingOptionsFn()
 
 #Definitions
-n_states = 81
-n_outputs = 9
+n_states = 73
+n_outputs = 6
 n_inputs = 4
 
 # x_dot = A x + B u
@@ -22,55 +23,40 @@ n_inputs = 4
 # 4 input u = {lon,lat,col,ped}
 # v1 C (6x81) 81 states, 6 output y = {q,p,r,phi,psi,theta} u,v,w are missing
 # v2 C (9x81) 81 states, 9 output y = {q,p,r,phi,psi,theta,u,v,w}
+inputsList = ['lon','lat','col','ped']
 outputsList = ['q', 'p', 'r','$\phi$','$\psi$', '$\\theta$']
 outputsUnitsDict = ['[rad/s]', '[rad]']
 
-A_from_FlightLab = np.loadtxt(cwd+'\\'+'fromFlightPhysics\\FL_LTI_models\\May2017RevAA_BE_0ft_15degC_2800kg_3.37m_147.tab', skiprows = 34)
-B_from_FlightLab = np.loadtxt('B_mat_from_FL_v1.txt')
-C_from_FlightLab = np.loadtxt('C_mat_from_FL_v1.txt')
-
-#Reshape imported matrix from txt file
-A = np.hstack([mat.reshape(n_states, 1) for mat in np.vsplit(A_from_FlightLab, n_states)])
-B = np.hstack([mat.reshape(n_states, 1) for mat in np.vsplit(B_from_FlightLab, n_inputs)])
-C = np.vstack([mat.reshape(1, n_states) for mat in np.vsplit(C_from_FlightLab, n_outputs)])
-D = np.zeros([n_outputs, n_inputs])
-
-np.savetxt('A.txt', A, delimiter = ',' ,fmt='%5.1f')
-np.savetxt('B.txt', B, delimiter = ',' ,fmt='%5.1f')
-np.savetxt('C.txt', C, delimiter = ',' ,fmt='%5.1f')
-np.savetxt('D.txt', D, delimiter = ',' ,fmt='%5.1f')
-
-print('A, shape: '+str(A.shape))
-print('B, shape: '+str(B.shape))
-print('C, shape: '+str(C.shape))
+A = np.loadtxt(cwd+'\\'+'fromFlightPhysics\\FL_LTI_models\\A_from_LTI.txt')
+B = np.loadtxt(cwd+'\\'+'fromFlightPhysics\\FL_LTI_models\\B_from_LTI.txt')
+C = np.loadtxt(cwd+'\\'+'fromFlightPhysics\\FL_LTI_models\\C_from_LTI.txt')
+D = np.loadtxt(cwd+'\\'+'fromFlightPhysics\\FL_LTI_models\\D_from_LTI.txt')
 
 sh09 = control.ss(A, B, C, D)
 
-# pdb.set_trace()
-
 # Set time
-T_in = np.asarray(np.linspace(0, 1, 100, endpoint=True))
-youts = []
+T_in = np.asarray(np.linspace(0, 3, 100, endpoint=True))
 
+youts = []
 for i_input in range(n_inputs):
 
 	T, yout = control.step_response(sh09, T = T_in, input = i_input, output = None)
 
 	youts += [yout, ]
 
-figure, axs = plt.subplots(6, n_inputs)
+figure, axs = plt.subplots(6, n_inputs, sharex = 'col')
 figure.set_size_inches(10, 14, forward=True)
+
 
 i_input = 0
 for yout in youts:
 	
-	i_out = 0
 	for i_out in range(6):
-		print('ax'+str(i_out) +str(i_input))
+
+		axs[i_out, i_input].plot(T, yout[i_out, :], linestyle = '-',  c = plotSettings['colors'][0], **plotSettings['line'])
+
 		axs[i_out, i_input].tick_params(axis='both', **plotSettings['axesTicks'])
-
-		axs[i_out, i_input].plot(T, yout[i_input, :], linestyle = '-',  c = plotSettings['colors'][0], **plotSettings['line'])
-
+		
 		#Plotting axes
 		if i_input == 0:
 			axs[i_out, i_input].set_ylabel('To: '+outputsList[i_out]+outputsUnitsDict[i_out//3], **plotSettings['axes_y'])
@@ -78,11 +64,13 @@ for yout in youts:
 		if i_out == 5:
 			axs[i_out, i_input].set_xlabel('t [s]', **plotSettings['axes_x'])
 
+		elif i_out == 0:
+			axs[i_out, i_input].set_title('From: '+inputsList[i_input], **plotSettings['title'])
+
 		axs[i_out, i_input].grid(which='both', **plotSettings['grid'])
-		i_out += 1
 
 	i_input += 1
 
 # ax.legend(**plotSettings['legend'])
 
-# plt.show(block = True)
+plt.show(block = True)
