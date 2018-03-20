6b456b0d8c3a86cff9d1a5368b4eb0b3df31dc82
Alejandro Valverde <alejandrovalverdelopez@gmail.com>
2018-03-20 10:55:02 +0100

    after arrival to Zurich

--- SH09_s_id/main_F16.m
+++ SH09_s_id/main_F16.m
@@ -1,5 +1,7 @@
 % Variables output from the model
-
+%
+%
+%
 % State vector elements are states:
 %        x(1)  = true airspeed, vt  (fps). 
 %        x(2)  = sideslip angle, beta  (rad).
@@ -15,6 +17,17 @@
 %        x(12) = h   (ft)  
 %        x(13) = pow (percent, 0 <= pow <= 100)
 
+magnitudesNames = {'vt',  'beta', 'alpha', 'p',     'q',     'r',     'phi', 'theta', 'psi',  'xe',   'ye',   'h',  'pow'};
+magnitudesUnits = {'fps', 'rad',  'rad',   'rad/s', 'rad/s', 'rad/s', 'rad', 'rad',   'rad',  'fte',  'fte',  'ft', 'percent'};
+
+magnitudes = cell(length(magnitudesNames), 1);
+
+for mag=1:length(magnitudesNames)
+
+  ts_temp = timeseries(states(:,1), states.Time, 'name', magnitudesNames{mag});
+  magnitudes{mag} = ts_temp;
+
+end
 
 % Forces
 % CX,CY,CZ,C1,Cm,Cn
@@ -27,3 +40,4 @@
 %        u(2) = stabilator input, stab  (deg).
 %        u(3) = aileron input, ail  (deg).
 %        u(4) = rudder input, rdr  (deg)
+
