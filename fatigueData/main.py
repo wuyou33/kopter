81116f828edb290bf4849bf77f1f60c6d9b913cd
Alejandro Valverde <valverdea@marenco.local>
2018-03-27 17:12:49 +0200

    plot fatigue data 2

--- fatigueData/main.py
+++ fatigueData/main.py
@@ -1,68 +1,84 @@
 import os
 import sys
 import pdb #pdb.set_trace()
+import getopt
+
+from moduleFunctions import *
+
+#Read postProc folder name from CMD
+CMDoptionsDict = {}
+CMDoptionsDict = readCMDoptionsMainAbaqusParametric(sys.argv[1:], CMDoptionsDict)
 
 # Import settings
-from moduleFunctions import *
 plotSettings = importPlottingOptions()
 cwd = os.getcwd() #Get working directory
 
 # What to do?
-actuatorFlag = True
-gaugesFlag = True
+gaugesFlag = CMDoptionsDict['dmsFlag']
+actuatorFlag = CMDoptionsDict['actuatorFlag']
 
 # Gauges data analysis
-testFrequency = 6.0 #HZ
+if gaugesFlag:
+	print('\n'+'**** Running data analysis program for calibrated strain gauges measurements'+'\n')
 
-dataMainPitchLink = dataFromGaugesSingleMagnitudeClass('PitchLinkMain', testFrequency)
-dataInnerPitchLink = dataFromGaugesSingleMagnitudeClass('InnerPitchLink', testFrequency)
-# dataClasses = (dataMainPitchLink, dataInnerPitchLink)
-dataClasses = (dataMainPitchLink,)
-inputFolderAddress = loadFileAddresses('filesToLoad_gauges.txt')
+	testFactor = 1000000.0 #HZ
+	variables = ('rs', 'min', 'max', 'mean')
 
-for folderName in inputFolderAddress.getTupleFiles(): #For each folder with min, max or mean values
-	os.chdir(folderName) #Go to folder
-	folderCurrent = os.getcwd() #Get working directory
-	fieldOfFiles = folderCurrent.split('\\')[-1]
+	dataMainPitchLink = dataFromGaugesSingleMagnitudeClass('PitchLinkMain', testFactor)
+	dataInnerPitchLink = dataFromGaugesSingleMagnitudeClass('InnerPitchLink', testFactor)
+	dataClasses = (dataMainPitchLink, dataInnerPitchLink)
+	inputFolderAddress = loadFileAddresses(CMDoptionsDict['fileNameOfFileToLoadFiles'])
 
-	listOfFilesSortedInFolder = sortFilesInFolderByLastNumberInName(os.listdir(folderCurrent))
+	for var in variables:
+		for folderName in inputFolderAddress.getTupleFiles(): #For each folder with min, max or mean values
+			os.chdir(folderName)
+			listOfFilesInFolderMathingVar = []
+			for fileName2 in os.listdir(folderName):
+				if fileName2.startswith(var):
 
+					listOfFilesInFolderMathingVar += [fileName2]
+
+			listOfFilesSortedInFolder = sortFilesInFolderByLastNumberInName(listOfFilesInFolderMathingVar)
+
+			for dataClass in dataClasses: #For each class variable
+				print('---> Importing data for variable: ' + dataClass.get_description() + ', '+var+ ' values')
+					
+				for fileName in listOfFilesSortedInFolder: #For each file matching the criteria
+
+					if dataClass.get_description() in fileName: #Restring to only file matching type of variable of class
+
+						print('-> Reading: ' + fileName)
+						dataClass.importDataForClass(fileName, var)
+						# dataClass.reStartXvalues()
+
+				dataClass.getTimeList(var)
+				
+				dataClass.reStartXvaluesAndLastID()
+
+	# Plotting
 	for dataClass in dataClasses: #For each class variable
+		# dataClass.plotMaxMinMean(plotSettings)
+		dataClass.plotResampled(plotSettings)
 
-		print('---> Importing data for variable: ' + dataClass.get_description() + ', '+fieldOfFiles+ ' values')
+	os.chdir(cwd)
 
-		for fileName in listOfFilesSortedInFolder: #For each file in folder
-
-			if dataClass.get_description() in fileName: #Restring to only file matching type of variable of class
-
-				print('-> Reading: ' + fileName)
-				dataClass.importDataForClass(fileName, fieldOfFiles)
-
-		dataClass.getTimeList(fieldOfFiles)
-		
-		dataClass.reStartXvalues()
-
-		# pdb.set_trace()
-
-# Plotting
-pdb.set_trace()
-for dataClass in dataClasses: #For each class variable
-	dataClass.plot(plotSettings)
-
-os.chdir(cwd)
 #Import data from actuator
 if actuatorFlag:
-	inputFilesAddress = loadFileAddresses('filesToLoad_actuator.txt')
+	print('\n'+'**** Running data analysis program for calibrated strain gauges measurements'+'\n')
+	inputFilesAddress = loadFileAddresses(CMDoptionsDict['fileNameOfFileToLoadFiles'])
 
 	dataFromRuns, previousNCycles = [], 0
 
 	for file in inputFilesAddress.getTupleFiles():
 
+		print('-> Reading: ' + file.split('\\')[-1])
 		dataFromRun_temp = importDataActuator(file)
 
 		dataFromRun_temp.setAbsoluteNCycles(previousNCycles)
 
 		previousNCycles = dataFromRun_temp.get_absoluteNCycles()[-1]
+
+		print('----> Last computed data point index: ' + str(int(previousNCycles)/1000000.0) + ' millions')
 
 		dataFromRuns += [dataFromRun_temp]
 
--- fatigueData/moduleFunctions.py
+++ fatigueData/moduleFunctions.py
@@ -4,8 +4,39 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import getopt
+import pdb #pdb.set_trace()
 
 ###### Functions
+def readCMDoptionsMainAbaqusParametric(argv, CMDoptionsDict):
+
+	short_opts = "o:f:"
+	long_opts = ["option=","fileName="]
+	try:
+		opts, args = getopt.getopt(argv,short_opts,long_opts)
+	except getopt.GetoptError:
+		raise ValueError('ERROR: Not correct input to script')
+
+	# check input
+	if len(opts) != len(long_opts):
+		raise ValueError('ERROR: Invalid number of inputs')	
+
+	for opt, arg in opts:
+
+		if opt in ("-f", "--fileName"):
+			# postProcFolderName = arg
+			CMDoptionsDict['fileNameOfFileToLoadFiles'] = arg
+
+		elif opt in ("-o", "--option"):
+			if arg.lower() in ('actuator', 'excel'):
+				CMDoptionsDict['actuatorFlag'] = True
+				CMDoptionsDict['dmsFlag'] = False
+			elif arg.lower() in ('dms', 'strain', 'gauge'):
+				CMDoptionsDict['actuatorFlag'] = False
+				CMDoptionsDict['dmsFlag'] = True
+
+	return CMDoptionsDict
+
 def sortFilesInFolderByLastNumberInName(listOfFiles):
 
 	a = []
@@ -103,7 +134,6 @@ def importDataActuator(fileName):
 
 	return dataFromRun
 
-
 def returnNumber(numStr):
 	
 	if numStr[-4] == '.':
@@ -130,10 +160,11 @@ def importPlottingOptions():
 	colors = ['k', 'b', 'y', 'm', 'r', 'c','k', 'b', 'y', 'm', 'r', 'c','k', 'b', 'y', 'm', 'r', 'c','k', 'b', 'y', 'm', 'r', 'c']
 	markers = ['o', 'v', '^', 's', '*', '+']
 	linestyles = ['-', '--', '-.', ':']
+	axes_ticks_n = {'x_axis' : 3} #Number of minor labels in between 
 
 	plotSettings = {'axes_x':axes_label_x,'axes_y':axes_label_y, 'title':text_title_properties,
 	                'axesTicks':axes_ticks, 'line':line, 'legend':legend, 'grid':grid, 'scatter':scatter,
-	                'colors' : colors, 'markers' : markers, 'linestyles' : linestyles}
+	                'colors' : colors, 'markers' : markers, 'linestyles' : linestyles, 'axes_ticks_n' : axes_ticks_n}
 
 	return plotSettings
 
@@ -161,6 +192,8 @@ class dataFromRunClass(object):
 		self.__absoluteNCycles = [d + previousNCycles_in for d in self.__cycleN]
 		self.__absoluteNCycles_mill = [(d + previousNCycles_in)/1000000 for d in self.__cycleN]
 
+	def get_id(self):
+		return self.__id
 	def get_absoluteNCycles(self):
 		return self.__absoluteNCycles
 	def get_absoluteNCycles_mill(self):
@@ -182,53 +215,58 @@ class dataFromGaugesSingleMagnitudeClass(object):
 
 	Class contaiting data from a certain run
 	"""
-	def __init__(self, description_in, freq_in):
+	def __init__(self, description_in, testFactor_in):
 		# super(dataFromGaugesSingleMagnitudeClass, self).__init__()
 
 		self.__description = description_in
-		self.__freq = freq_in
+		self.__testFactor = testFactor_in
 
 		self.__max = []
 		self.__mean = []
 		self.__min = []
+		self.__rs = []
 
 		self.__timeMax = []
 		self.__timeMean = []
 		self.__timeMin = []
+		self.__timeRs = []
 		self.__timeSecNewRunMax = []
 		self.__timeSecNewRunMean = []
 		self.__timeSecNewRunMin = []
+		self.__timeSecNewRunRs = []
 
+		self.__lastID = 0
 		self.__xValues = []
 		self.__xValuesNewRun = []
-		self.__lastID = 0
 
-	def reStartXvalues(self):
+	def reStartXvaluesAndLastID(self):
+		self.__lastID = 0
 		self.__xValues = []
 		self.__xValuesNewRun = []
 
 	def set_magData(self, nameField, data):
 
-		if nameField == 'mean':
+		if nameField == 'rs':
+			self.__rs += data
+		elif nameField == 'mean':
 			self.__mean += data
 		elif nameField == 'max':
 			self.__max += data
 		elif nameField == 'min':
 			self.__min += data
-
 		else:
 			raise ValueError('Error in identifying data field: ' + nameField)
-
 	def getTimeList(self, nameField):
 		
-		timeSec = np.linspace(0, int(len(self.__xValues)/self.__freq), len(self.__xValues), endpoint=True)
-		print(str(t)) for t in self.__xValuesNewRun
-		timeSecNewRun = [float(t/self.__freq) for t in self.__xValuesNewRun]
-		a = self.__xValuesNewRun
-		f = self.__freq
-		pdb.set_trace()
+		timeSec = np.linspace(0, float(len(self.__xValues)/self.__testFactor), len(self.__xValues), endpoint=True)
+		timeSecNewRun = [float(t/self.__testFactor) for t in self.__xValuesNewRun]
 
-		if nameField == 'mean':
+		print('----> Last computed time point: ' + str(timeSec[-1]) + ' millions')
+
+		if nameField == 'rs':
+			self.__timeRs = timeSec
+			self.__timeSecNewRunRs = timeSecNewRun
+		elif nameField == 'mean':
 			self.__timeMean = timeSec
 			self.__timeSecNewRunMean = timeSecNewRun
 		elif nameField == 'max':
@@ -246,28 +284,38 @@ class dataFromGaugesSingleMagnitudeClass(object):
 		file = open(fileName, 'r')
 		lines = file.readlines()
 
-		skipLines, dataID, data = 0, [], []
+		skipLines, dataID, data, counter = 0, [], [], 0
 
 		for line in lines[(skipLines+1):]:
 
 			data += [float(cleanString(line))]
 
-			dataID = dataID + [self.__lastID+1]
-				
+			if counter == 0: #First iteration
+				dataID += [self.__lastID+1]
+			else:
+				dataID += [dataID[-1]+1]
+
+			counter += 1
+		
 		file.close()
 
 		self.set_magData(fieldOfFile, data)
 
 		self.__lastID = dataID[-1]
 
-		self.__xValuesNewRun += [dataID[-1]]
+		self.__xValuesNewRun += [dataID[-1],]
 
-		print('\t'+'\t'+'-> Last computed data point absolute index: ' + str(dataID[-1]))
+		print('\t'+'-> Last computed data point absolute index: ' + str(dataID[-1]/1000000.0) + ' millions')
 
 		self.__xValues += dataID
 	
 	def get_description(self):
 		return self.__description
+
+	def get_xValues(self):
+		return self.__xValues
+	def get_xValuesNewRun(self):
+		return self.__xValuesNewRun
 
 	def get_max(self):
 		return self.__max
@@ -275,6 +323,8 @@ class dataFromGaugesSingleMagnitudeClass(object):
 		return self.__mean
 	def get_min(self):
 		return self.__min
+	def get_rs(self):
+		return self.__rs
 
 	def get_timeMax(self):
 		return self.__timeMax
@@ -282,6 +332,8 @@ class dataFromGaugesSingleMagnitudeClass(object):
 		return self.__timeMean
 	def get_timeMin(self):
 		return self.__timeMin
+	def get_timeRs(self):
+		return self.__timeRs
 
 	def get_timeSecNewRunMax(self):
 		return self.__timeSecNewRunMax
@@ -289,13 +341,13 @@ class dataFromGaugesSingleMagnitudeClass(object):
 		return self.__timeSecNewRunMean
 	def get_timeSecNewRunMin(self):
 		return self.__timeSecNewRunMin
+	def get_timeSecNewRunRs(self):
+		return self.__timeSecNewRunRs
 
-	def plot(self, plotSettings):
+	def plotMaxMinMean(self, plotSettings):
 
 		figure, ax = plt.subplots(1, 1)
-		ax.grid(which='both', **plotSettings['grid'])
 		figure.set_size_inches(10, 6, forward=True)
-		ax.tick_params(axis='both', **plotSettings['axesTicks'])
 
 		ax.plot(self.get_timeMax(), self.get_max(), linestyle = '', marker = '+', c = plotSettings['colors'][0], label = 'Max force', **plotSettings['line'])
 		ax.plot(self.get_timeMean(), self.get_mean(), linestyle = '', marker = '+', c = plotSettings['colors'][1], label = 'Mean force', **plotSettings['line'])
@@ -303,50 +355,82 @@ class dataFromGaugesSingleMagnitudeClass(object):
 
 		#Division line for runs
 		for div in self.get_timeSecNewRunMean():
-			ax.plot(2*[div], [self.get_min()[-1]*1.2, self.get_max()[-1]*1.2], linestyle = '--', marker = '', c = plotSettings['colors'][4], **plotSettings['line'])
+			ax.plot(2*[div], [self.get_min()[-1]*1.2, self.get_max()[-1]*1.2], linestyle = '-', marker = '', c = plotSettings['colors'][4], **plotSettings['line'])
 
-		ax.set_xlabel('Time uninterrupted test [seconds]', **plotSettings['axes_x'])
+		ax.set_xlabel('Number of cycles [Millions]', **plotSettings['axes_x'])
 		ax.set_ylabel('Force [kN]', **plotSettings['axes_y'])
+		
 		ax.legend(**plotSettings['legend'])
 		ax.set_title(self.get_description(), **plotSettings['title'])
+
+		#Figure settings
+		ax.grid(which='both', **plotSettings['grid'])
+		ax.tick_params(axis='both', which = 'both', **plotSettings['axesTicks'])
+		ax.minorticks_on()
+
+	def plotResampled(self, plotSettings):
+
+		figure, ax = plt.subplots(1, 1)
+		figure.set_size_inches(10, 6, forward=True)
+
+		ax.plot(self.get_timeRs(), self.get_rs(), linestyle = '', marker = '+', c = plotSettings['colors'][0], label = 'Measured force, re-sampled 100Hz', **plotSettings['line'])
+
+		#Division line for runs
+		for div in self.get_timeSecNewRunRs():
+			ax.plot(2*[div], [min(self.get_rs())*1.2, max(self.get_rs())*1.2], linestyle = '-', marker = '', c = plotSettings['colors'][4], **plotSettings['line'])
+
+		ax.set_xlabel('Number of cycles [Millions]', **plotSettings['axes_x'])
+		ax.set_ylabel('Force [kN]', **plotSettings['axes_y'])
+
+		#Legend and title
+		ax.legend(**plotSettings['legend'])
+		ax.set_title(self.get_description(), **plotSettings['title'])
+
+		#Figure settings
+		ax.grid(which='both', **plotSettings['grid'])
+		ax.tick_params(axis='both', which = 'both', **plotSettings['axesTicks'])
+		ax.minorticks_on()
 
 def plotSingleRun(dataFromRun, plotSettings):
 
 	figure, ax = plt.subplots(1, 1)
-	ax.grid(which='both', **plotSettings['grid'])
 	figure.set_size_inches(10, 6, forward=True)
-	ax.tick_params(axis='both', **plotSettings['axesTicks'])
 
-	ax.plot(dataFromRun.get_cycleN(), dataFromRun.get_maxF(), linestyle = '', marker = '+', c = plotSettings['colors'][0], label = 'Max force', **plotSettings['line'])
-	ax.plot(dataFromRun.get_cycleN(), dataFromRun.get_meanF(), linestyle = '', marker = '+', c = plotSettings['colors'][1], label = 'Mean force', **plotSettings['line'])
-	ax.plot(dataFromRun.get_cycleN(), dataFromRun.get_minF(), linestyle = '', marker = '+', c = plotSettings['colors'][2], label = 'Min force', **plotSettings['line'])
+	ax.plot(dataFromRun.get_cycleN(), dataFromRun.get_maxF(), linestyle = '-', marker = '', c = plotSettings['colors'][0], label = 'Max force', **plotSettings['line'])
+	ax.plot(dataFromRun.get_cycleN(), dataFromRun.get_meanF(), linestyle = '-', marker = '', c = plotSettings['colors'][1], label = 'Mean force', **plotSettings['line'])
+	ax.plot(dataFromRun.get_cycleN(), dataFromRun.get_minF(), linestyle = '-', marker = '', c = plotSettings['colors'][2], label = 'Min force', **plotSettings['line'])
 
-	ax.set_xlabel('Number of cycles', **plotSettings['axes_x'])
+	ax.set_xlabel('Number of cycles [Millions]', **plotSettings['axes_x'])
 	ax.set_ylabel('Force [kN]', **plotSettings['axes_y'])
+
 	ax.legend(**plotSettings['legend'])
+	ax.set_title('Results from Run #'+str(dataFromRun.get_id()), **plotSettings['title'])
+
+	#Figure settings
+	ax.grid(which='both', **plotSettings['grid'])
+	ax.tick_params(axis='both', which = 'both', **plotSettings['axesTicks'])
+	ax.minorticks_on()
 
 def plotAllRuns(dataFromRuns, plotSettings):
 
 	def threePlotForRun(dataFromRun, plotSettings, ax):
 		
-		ax.plot(dataFromRun.get_absoluteNCycles_mill(), dataFromRun.get_maxF(), linestyle = '', marker = '+', c = plotSettings['colors'][0], **plotSettings['line'])
-		ax.plot(dataFromRun.get_absoluteNCycles_mill(), dataFromRun.get_meanF(), linestyle = '', marker = '+', c = plotSettings['colors'][1], **plotSettings['line'])
-		ax.plot(dataFromRun.get_absoluteNCycles_mill(), dataFromRun.get_minF(), linestyle = '', marker = '+', c = plotSettings['colors'][2], **plotSettings['line'])	
+		ax.plot(dataFromRun.get_absoluteNCycles_mill(), dataFromRun.get_maxF(), linestyle = '-', marker = '', c = plotSettings['colors'][0], **plotSettings['line'])
+		ax.plot(dataFromRun.get_absoluteNCycles_mill(), dataFromRun.get_meanF(), linestyle = '-', marker = '', c = plotSettings['colors'][1], **plotSettings['line'])
+		ax.plot(dataFromRun.get_absoluteNCycles_mill(), dataFromRun.get_minF(), linestyle = '-', marker = '', c = plotSettings['colors'][2], **plotSettings['line'])	
 	
-	def plotDivisionLine(dataFromLastRun, ax, plotSettings):
-
-		ax.plot(2*[dataFromLastRun.get_absoluteNCycles_mill()[-1]], [dataFromLastRun.get_minF()[-1]*1.2, dataFromLastRun.get_maxF()[-1]*1.2], linestyle = '--', marker = '', c = plotSettings['colors'][4], **plotSettings['line'])
-
 	figure, ax = plt.subplots(1, 1)
 	figure.set_size_inches(10, 6, forward=True)
 
+	#Plot first division line
+	ax.plot(2*[0.0], [dataFromRuns[0].get_minF()[-1]*1.2, dataFromRuns[0].get_maxF()[-1]*1.2], linestyle = '--', marker = '', c = plotSettings['colors'][4], **plotSettings['line'])
 	for data in dataFromRuns:
 
 		threePlotForRun(data, plotSettings, ax)
-		plotDivisionLine(data, ax, plotSettings)
+		
+		#Plot division lines
+		ax.plot(2*[data.get_absoluteNCycles_mill()[-1]], [data.get_minF()[-1]*1.2, data.get_maxF()[-1]*1.2], linestyle = '--', marker = '', c = plotSettings['colors'][4], **plotSettings['line'])
 
-	ax.grid(which='both', **plotSettings['grid'])
-	ax.tick_params(axis='both', **plotSettings['axesTicks'])
 	ax.set_xlabel('Number of cycles [Millions]', **plotSettings['axes_x'])
 	ax.set_ylabel('Force [kN]', **plotSettings['axes_y'])
 
@@ -358,3 +442,41 @@ def plotAllRuns(dataFromRuns, plotSettings):
 	legendHandles = legendHandles + [handle0, handle1, handle2]
 	ax.legend(handles = legendHandles, **plotSettings['legend'])
 
+	#Title
+	ax.set_title('Results fatigue test', **plotSettings['title'])
+
+	#Figure settings
+	ax.grid(which='both', **plotSettings['grid'])
+
+	#Tick parametersget_xticks
+	# majorTicks = ax.get_xmajorticklabels()
+	ax.minorticks_on()
+	xticksMajor = ax.get_xticks(minor = False)
+	xticksMinor = ax.get_xticks(minor = True)
+	yticksMinor = ax.get_yticks(minor = True)
+	[print(m) for m in xticksMajor]
+	print('-')
+	[print(m) for m in xticksMinor]
+	print('-')
+	# majorTicksNum = [float(m.get_text()) for m in majorTicks]
+
+	xticksMinorUser = []
+	counter = 0
+	for tick in xticksMajor[: len(xticksMajor)-2]:
+		xticksMinorUserInBetween = np.linspace(tick, xticksMajor[counter+1], plotSettings['axes_ticks_n']['x_axis']+1, endpoint=False)
+		[print(m) for m in xticksMinorUserInBetween]
+		print('-')
+		xticksMinorUser += xticksMinorUserInBetween
+		counter += 1
+
+	[print(m) for m in xticksMinorUser]
+	xlistOfLabelsMinor = [str(m) for m in xticksMinor]
+	ylistOfLabelsMinor = [str(m) for m in yticksMinor]
+
+	ax.set_xticklabels(xlistOfLabelsMinor, minor=True)
+	ax.set_yticklabels(ylistOfLabelsMinor, minor=True)
+
+	ax.tick_params(axis='both', which = 'both', **plotSettings['axesTicks'])
+	####
+
+	
